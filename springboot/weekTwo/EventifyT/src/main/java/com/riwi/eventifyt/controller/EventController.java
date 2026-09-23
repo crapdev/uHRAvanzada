84b1a748bce73d9f5afbc1a28c15c2a0baf890b8
crapdev <calborparra@gmail.com>
2026-09-23 11:50:54 -0500

    feat: exception end event controller

--- springboot/weekTwo/EventifyT/src/main/java/com/riwi/eventifyt/controller/EventController.java
+++ springboot/weekTwo/EventifyT/src/main/java/com/riwi/eventifyt/controller/EventController.java
@@ -1,35 +1,56 @@
 package com.riwi.eventifyt.controller;
 
+import com.riwi.eventifyt.DTO.EventRequest;
+import com.riwi.eventifyt.DTO.EventResponse;
 import com.riwi.eventifyt.domain.Event;
 import com.riwi.eventifyt.service.EventService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RequiredArgsConstructor
 @RestController //  Esta clase va a recibir peticiones HTTP y sus respuestan se devolveran en JSON     Combina @Controller y @ResponseBody
 @RequestMapping("/api/events") //Define la ruta
 @Tag(name = "Events", description = "Operaciones para registrar y consultar eventos") //con esto Swagger agrupa los endpoints como Events
 public class EventController {
     private final EventService eventService;
 
-    public EventController(EventService eventService){ this.eventService = eventService;}
 
-    @PostMapping
-    @Operation(summary = "Registrar un nuevo evento", description = "holaaa")
-    public ResponseEntity<Event> create(@RequestBody Event event){ // el @Requestbody obtiene el json del cliente y lo convierte en un objeto java
-        Event createdEvent = eventService.create(event);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
+    @GetMapping
+    @Operation(summary = "Obtener eventos")
+    public ResponseEntity<List<EventResponse>> listAll(){
+        return ResponseEntity.ok(eventService.listAll());
     }
 
     @GetMapping
-    @Operation(summary = "Obtener eventos", description = "holaaa")
-    public ResponseEntity<List<Event>> listAll(){
-        List<Event> events = eventService.listAll();
-        return ResponseEntity.ok(events);
+    @Operation(summary = "Obtener un evento por ID")
+    public ResponseEntity<EventResponse> findById(@PathVariable Long id){
+        return ResponseEntity.ok(eventService.findById(id));
+    }
+
+    @PostMapping
+    @Operation(summary = "Registrar un nuevo evento")
+    public ResponseEntity<EventResponse> create(@Valid @RequestBody EventRequest request){ // el @Requestbody obtiene el json del cliente y lo convierte en un objeto java
+        EventResponse created = eventService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping
+    @Operation(summary = "Actualizar un evento existente")
+    public ResponseEntity<EventResponse>   update(@PathVariable Long id, @Valid @RequestBody EventRequest request){
+        return ResponseEntity.ok(eventService.update(id, request));
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Eliminar un evento")
+    public ResponseEntity<void> delete (@PathVariable Long id){
+        eventService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- springboot/weekTwo/EventifyT/src/main/java/com/riwi/eventifyt/exception/GlobalExceptionHandler.java
+++ springboot/weekTwo/EventifyT/src/main/java/com/riwi/eventifyt/exception/GlobalExceptionHandler.java
@@ -16,4 +16,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
+    // Se ejecuta cuando ocurra un ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(ResourceNotFoundException exception){
+        Map<String, String> error = Map.of("error", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
